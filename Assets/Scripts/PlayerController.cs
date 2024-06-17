247f88966d318275944899c4165763bdd453a49b
Mukesh <167955129+0xmukeshr@users.noreply.github.com>
2024-06-17 22:14:47 +0530

    Added tilemaps and player movements

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 1f;
+
+    private PlayerControls playerControls;
+    private Vector2 movement;
+    private Rigidbody2D rb;
+    private Animator myAnimator;
+    private SpriteRenderer mySpriteRender;
+
+
+    private void Awake() {
+        playerControls = new PlayerControls();
+        rb = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        mySpriteRender = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnEnable() {
+        playerControls.Enable();
+    }
+
+    private void Update() {
+        PlayerInput();
+    }
+
+    private void FixedUpdate() {
+        AdjustPlayerFacingDirection();
+        Move();
+    }
+
+    private void PlayerInput() {
+        movement = playerControls.Movement.Move.ReadValue<Vector2>();
+
+        myAnimator.SetFloat("moveX", movement.x);
+        myAnimator.SetFloat("moveY", movement.y);
+    }
+
+    private void Move() {
+        rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
+    }
+
+    private void AdjustPlayerFacingDirection() {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(transform.position);
+
+        if (mousePos.x < playerScreenPoint.x) {
+            mySpriteRender.flipX = true;
+        } else {
+            mySpriteRender.flipX = false;
+        }
+    }
+}
