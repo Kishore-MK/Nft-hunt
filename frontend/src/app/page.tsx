a9b8e53932d7c561feb6d053df504518447d04bd
Kishore <tamiltac6@gmail.com>
2024-06-20 00:15:39 +0530

    Landing page boilerplate setup

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import WalletBar from "@/components/WalletBar";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen gap-12">
+      <WalletBar />
+    </main>
+  );
+}
